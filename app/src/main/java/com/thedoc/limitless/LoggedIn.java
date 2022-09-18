764d9e3c35bbb850227373c6c650354d1e023c8c
Thedoc Scripts <82754205+thedocscripts@users.noreply.github.com>
2022-09-18 20:43:42 +0200

    update

--- app/src/main/java/com/thedoc/limitless/LoggedIn.java
+++ app/src/main/java/com/thedoc/limitless/LoggedIn.java
@@ -2,18 +2,32 @@ package com.thedoc.limitless;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class LoggedIn extends AppCompatActivity {
     FirebaseAuth mAuth;
+    FirebaseUser user;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +36,76 @@ public class LoggedIn extends AppCompatActivity {
 
 
         mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
-
-
-
-        Button ss = (Button) findViewById(R.id.button);
-
+        user = mAuth.getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         TextView tt = (TextView) findViewById(R.id.s2tt);
         //
         tt.setText("Bienvenido " + user.getEmail().toString());
 
-        ss.setOnClickListener(new View.OnClickListener() {
+
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent myIntent = new Intent(LoggedIn.this, MainActivity.class);
-                LoggedIn.this.startActivity(myIntent);
+                addItem(view);
             }
         });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Item Checked!", Toast.LENGTH_LONG).show();
+
+                view.setBackgroundColor(context.getResources().getColor(R.color.green));
+                itemsAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void addItem(View view) {
+        EditText input = findViewById(R.id.AdditemInp);
+        String itemText = input.getText().toString();
+
+        if(!itemText.isEmpty()){
+            itemsAdapter.add("Author: " + user.getEmail() + " \nItem: " + itemText);
+
+
+            System.out.println(itemsAdapter.getCount());
+
+
+
+
+
+
+            input.setText("");
+        }else{
+            Toast.makeText(getApplicationContext(), "Please add an item", Toast.LENGTH_LONG
+            ).show();
+        }
     }
 }
